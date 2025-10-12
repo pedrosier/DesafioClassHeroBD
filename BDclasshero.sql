8f1b036ed3d3cafc04aa92c761c6f4c650bdc6a3
pedrosier <pedrosier001@gmail.com>
2025-10-12 19:40:05 -0300

    Update BDclasshero.sql

--- BDclasshero.sql
+++ BDclasshero.sql
@@ -1,6 +1,2 @@
-SELECT 
-    funcionario_id,
-    nome,
-    MOD(ROW_NUMBER() OVER (ORDER BY funcionario_id) - 1, 4) + 1 AS equipe
-FROM 
-    funcionarios;
+ALTER TABLE Produto
+ADD CONSTRAINT uq_codigo UNIQUE (codigo);
